f32558964ce8f0af0629da9d2ea3fb05f2319138
Your Name <you@example.com>
2024-03-09 15:17:03 +0200

    Добавьте файлы проекта.

---
+++ TransportRent/Program.cs
@@ -0,0 +1,116 @@
+﻿//Name Type
+//Caterpillar bulldozer Heavy
+//Kamaz truck Regular
+//Komatsu crane Heavy
+//Volvo steamroller Regular
+//Bosch jackhammer Specialized
+
+//There are three different fees:
+//One - time rental fee - 100 EUR
+//Premium daily fee - 60 EUR/day
+//Regular daily fee - 40EUR/day
+
+
+
+//interface Transport
+//{  }
+
+class Client
+{
+    public int loyalityPoint = 0;
+    public double Purchase(string productType, int days)//Product product
+    {
+
+        double premiumPrice = 60;
+        double regularPrice = 40;
+        double finalprice = 0;
+        double oneTimerental = 100;
+        if (productType == "Heavy") //product.productType == "Heavy"
+        {
+            finalprice = oneTimerental + (premiumPrice * days);
+            loyalityPoint += 2;
+        }
+        else if (productType == "Regular")
+        {
+            if (days > 3)
+            {
+                finalprice = oneTimerental + (premiumPrice * 2) + (regularPrice * (days - 2));
+            }
+            else if (days <= 3)
+            {
+                finalprice = oneTimerental + (premiumPrice * days);
+            }
+
+            loyalityPoint += 1;
+        }
+        else if (productType == "Specialized")
+        {
+            if (days > 3)
+            {
+                finalprice = (premiumPrice * 3) + (regularPrice * (days - 3));
+            }
+            else if (days <= 3)
+            {
+                finalprice = premiumPrice * days;
+            }
+            loyalityPoint += 1;
+        }
+
+
+
+
+        return finalprice;
+    }
+}
+
+
+
+
+abstract class Product
+{
+
+
+
+    private string? productType;
+
+    public string ProductType
+    {
+        get { return productType; }
+        set { productType = value; }
+    }
+
+
+}
+class Bulldozer : Product
+{
+
+    private string? productType = "Heavy";
+
+
+
+}
+class Truck
+{ }
+
+class Crane
+{ }
+
+class Steamroller
+{ }
+
+class Jackhammer
+{ }
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Client client = new Client();
+
+        Console.WriteLine(client.Purchase("Heavy", 3));
+        Console.WriteLine(client.Purchase("Regular", 5));
+        Console.WriteLine(client.Purchase("Specialized", 5));
+        Console.WriteLine(client.loyalityPoint);//4
+    }
+}
